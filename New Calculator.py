31d2b7c17bf405286f51bfae42253762af50bdf0
Hardik Joshi <163398638+Kioskar1@users.noreply.github.com>
2024-03-15 17:29:01 +0530

    Add files via upload

---
+++ New Calculator.py
@@ -0,0 +1,25 @@
+a = float(input('Enter a number :'))
+b = float(input('Enter another number :'))
+
+if type(a) or type(b) != float:
+    raise ValueError('Invalid input')
+else:
+    pass
+
+op = input('Operation :')
+
+match op :
+    case 'add' :
+        print(a + b)
+
+    case'subtract' :
+        print(a - b)
+
+    case'multiply' :
+        print(a * b)
+
+    case 'divide' :
+        print(a / b)
+
+    case _ :
+        raise ValueError('Invalid operation')
